e16e1f533b1b1051f7c88f861f81b5528e31e4a2
Yusuf Islamov <wysyfimam3@gmail.com>
2021-09-17 20:20:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+  int iscost = 10_000;
+  int BonusMil = 20;
+  int NumberMiles = iscost / BonusMil;
+        System.out.printf(NumberMiles + "miles");
+
+
+    }
+}
